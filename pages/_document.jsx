1097f93e3b17386820d6f9fcd1b856eca9ff6001
Daniel Magro <dando385@yahoo.com>
2024-04-07 15:05:57 -0400

    deleted .next and rebuild

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,7 +6,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          {/* Added crossOrigin="true" to specify the attribute value correctly */}
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet" />
         </Head>
         <body>
