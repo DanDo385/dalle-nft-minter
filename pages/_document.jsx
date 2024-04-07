1cec5cfe119a702b388d3358d69d3e157a90fb95
Daniel Magro <dando385@yahoo.com>
2024-04-07 14:23:43 -0400

    fixing api route

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet" />
         </Head>
         <body>
